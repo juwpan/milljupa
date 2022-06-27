550068fe7fb5bb3102b0c32af6e515defc62eaf1
juwpan <juwpan@gmail.com>
2022-06-27 08:29:06 +0400

    not work display error mesage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-// import * as ActiveStorage from "@rails/activestorage/"
-// ActiveStorage.start()
+import * as ActiveStorage from "@rails/activestorage/"
+ActiveStorage.start()
+// Turbo.session.drive = false
 
 import "@fortawesome/fontawesome-free"
 import * as bootstrap from "bootstrap"
 
 import "jquery"
+// import "./map"
+// import "./lightbox"
