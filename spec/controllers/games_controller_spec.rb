c066b64fe4ff0f6c4899fc26d956b134e3cf04c4
juwpan <juwpan@gmail.com>
2022-06-29 19:57:51 +0400

    incorrect answer

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -147,44 +147,58 @@ RSpec.describe GamesController, type: :controller do
         expect(response).to redirect_to(game_path(game))
         expect(flash.empty?).to be_truthy # удачный ответ не заполняет flash
       end
+
+      it 'incorrect answer' do
+        put :answer, params: { id: game_w_questions.id, letter: 'a' }
+
+        expect(game.finished?).to be_truthy
+        expect(game.current_level).to be 0
+        expect(response).to redirect_to user_path(user)     
+        expect(flash[:alert]).to be
+      end
     end
   end
 
-  #   # тест на отработку "помощи зала"
-  #   it 'uses audience help' do
-  #     # сперва проверяем что в подсказках текущего вопроса пусто
-  #     expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
-  #     expect(game_w_questions.audience_help_used).to be_falsey
+  describe '#help' do
+    # тест на отработку "помощи зала"
+    it 'uses audience help' do
+      # сперва проверяем что в подсказках текущего вопроса пусто
+      expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
+      expect(game_w_questions.audience_help_used).to be_falsey
 
-  #     # фигачим запрос в контроллен с нужным типом
-  #     put :help, params: { id: game_w_questions.id, help_type: :audience_help }
-  #     game = assigns(:game)
+      # фигачим запрос в контроллен с нужным типом
+      put :help, params: { id: game_w_questions.id, help_type: :audience_help }
+      game = assigns(:game)
 
-  #     # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
-  #     expect(game.finished?).to be_falsey
-  #     expect(game.audience_help_used).to be_truthy
-  #     expect(game.current_game_question.help_hash[:audience_help]).to be
-  #     expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
-  #     expect(response).to redirect_to(game_path(game))
-  #   end
+      # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+      expect(game.finished?).to be_falsey
+      expect(game.audience_help_used).to be_truthy
+      expect(game.current_game_question.help_hash[:audience_help]).to be
+      expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+      expect(response).to redirect_to(game_path(game))
+    end
 
-  #   it 'hint 50/50 can be used' do
-  #     expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
-  #     expect(game_w_questions.fifty_fifty_used).to be_falsey
+    it 'hint 50/50 can be used' do
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be_falsey
 
-  #     put :help, params: { id: game_w_questions.id, help_type: :fifty_fifty}
-  #     game = assigns(:game)
+      put :help, params: { id: game_w_questions.id, help_type: :fifty_fifty}
+      game = assigns(:game)
 
-  #     expect(game.finished?).to be_falsey
-  #     expect(game.fifty_fifty_used).to be_truthy
-  #     expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+      expect(game.finished?).to be_falsey
+      expect(game.fifty_fifty_used).to be_truthy
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to be
 
-  #     variants = game.current_game_question.help_hash[:fifty_fifty]
+      variants = game.current_game_question.help_hash[:fifty_fifty]
 
-  #     expect(variants).to include('d')
-  #     expect(variants.size).to eq(2)
-  #     expect(response).to redirect_to(game_path(game))
-  #   end
+      expect(variants).to include('d')
+      expect(variants.size).to eq(2)
+      expect(response).to redirect_to(game_path(game))
+    end
+
+    it '' do
+    end
+  end
 
   #   it 'take money' do
   #     game_w_questions.update_attribute(:current_level, 2)
@@ -200,45 +214,4 @@ RSpec.describe GamesController, type: :controller do
 
   #     expect(response).to redirect_to(user_path(user))
   #     expect(flash[:warning]).to be
-  #   end
-
-
-  #   it 'creates game' do
-  #     # сперва накидаем вопросов, из чего собирать новую игру
-  #     generate_questions(15)
-
-  #     post :create
-  #     game = assigns(:game) # вытаскиваем из контроллера поле @game
-
-  #     # проверяем состояние этой игры
-  #     expect(game.finished?).to be_falsey
-  #     expect(game.user).to eq(user)
-  #     # и редирект на страницу этой игры
-  #     expect(response).to redirect_to(game_path(game))
-  #     expect(flash[:notice]).to eq(1)
-  #   end
-
-
-  #   # Redirected to http://localhost:3000/games/6
-  #   # Filter chain halted as :goto_game_in_progress! rendered or redirected
-  #   # Completed 302 Found in 3ms (ActiveRecord: 0.2ms | Allocations: 1162)
-
-
-  #   # Started GET "/games/6" for ::1 at 2022-06-29 01:31:25 +0400
-  #   # Processing by GamesController#show as TURBO_STREAM
-
-  #   it 'try to create second game' do
-  #     # убедились что есть игра в работе
-  #     expect(game_w_questions.finished?).to be_falsey
-    
-  #     # отправляем запрос на создание, убеждаемся что новых Game не создалось
-  #     expect { post :create }.to change(Game, :count).by(0)
-    
-  #     game = assigns(:game) # вытаскиваем из контроллера поле @game
-  #     expect(game).to be_nil
-    
-  #     # и редирект на страницу старой игры
-  #     expect(response).to redirect_to(game_path(game_w_questions))
-  #     expect(flash[:alert]).to be
-  #   end
 end
