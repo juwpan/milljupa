8b969c5a8f23af04a819fd79fdc08c3ab05d3cf7
juwpan <juwpan@gmail.com>
2022-07-02 23:19:41 +0400

    rspec test views user/show

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -4,20 +4,62 @@ require 'support/factory_bot'
 RSpec.describe 'users/show', type: :view do
 
   describe '#show.html' do
-    before(:each) do
-      assign(:user, create(:user, name: 'Вафлей', balance: 2000))
+    context 'when user log in' do   
+      let(:user) { create(:user, name:'Вова') }
+      let(:game) { build_stubbed(
+        :game, id: 15, created_at: Time.parse('2016.10.09, 13:00'), current_level: 10, prize: 1000
+      )}
 
-      render
+      before(:each) do
+        assign(:user, user)
+        sign_in user
+
+        allow(game).to receive(:status).and_return(:in_progress)
+        render partial: 'users/game', object: game
+
+        render
+      end
+
+      it 'renders player names' do
+        expect(rendered).to match 'Вова'
+      end
+
+      it 'user sees the button' do
+        expect(rendered).to match 'Сменить имя и пароль'
+      end
+
+      it 'renders game id' do
+        expect(rendered).to match '15'
+      end
+  
+      it 'renders game start time' do
+        expect(rendered).to match '09 окт., 12:00'
+      end
+  
+      it 'renders game current question' do
+        expect(rendered).to match '10'
+      end
+  
+      it 'renders game status' do
+        expect(rendered).to match 'в процессе'
+      end
+  
+      it 'renders game prize' do
+        expect(rendered).to match '1 000 ₽'
+      end
     end
 
-    # Этот сценарий проверяет, что шаблон выводит имя игрока
-    it 'renders player names' do
-      expect(rendered).to match 'Вафлей'
-    end
+    context 'when the user logged out' do
+      let(:user) { create(:user, name:'Федот') }
 
-    # # Этот сценарий проверяет, что юзеры в нужном порядке
-    # it 'renders player names in right order' do
-    #   expect(rendered).to match /Вадик.*Миша/m
-    # end
+      before(:each) do
+        assign(:user, user)
+        render
+      end
+
+      it 'user does not see button' do
+        expect(rendered).not_to match 'Сменить имя и пароль'
+      end
+    end
   end
 end
